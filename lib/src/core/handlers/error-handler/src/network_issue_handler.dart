88488c82439ed7ab8e21b7d7a448d88054b18575
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2025-05-01 18:58:00 +0300

    fix: issues with error handler

--- lib/src/core/handlers/error-handler/src/network_issue_handler.dart
+++ lib/src/core/handlers/error-handler/src/network_issue_handler.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:better_auth_client/src/core/exceptions/app_exceptions.dart';
+
 import '../error_handler.dart';
 import '../models/better_auth_exception.dart';
 
@@ -15,6 +17,11 @@ class NetworkIssueHandler extends BaseErrorHandler {
     } else if (error is TimeoutException) {
       return BetterAuthException(
         message: 'Request timed out',
+        code: BetterAuthExceptionCode.networkIssue,
+      );
+    } else if (error is NetworkException) {
+      return BetterAuthException(
+        message: error.message,
         code: BetterAuthExceptionCode.networkIssue,
       );
     } else {
