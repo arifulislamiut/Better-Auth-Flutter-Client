d59d4e680b09a13257ea8c5e54ee11ff2a41019f
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2025-05-01 22:42:28 +0300

    feat: expose local storage interface

--- lib/better_auth_client.dart
+++ lib/better_auth_client.dart
@@ -4,3 +4,4 @@ library;
 export 'src/auth_client.dart';
 export 'src/models/response/user.dart';
 export 'src/core/configs/enums/providers.dart';
+export './src/service/local-storage-service/better_auth_local_storage_service.dart';
