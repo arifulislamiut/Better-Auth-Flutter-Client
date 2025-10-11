d3883875d17cd7c6c4e178d97fb38dc8d1a20d06
Anexdev1 <anwarnasir0970@gmail.com>
2025-10-11 05:45:39 +0300

    feat: export TwoFactorClient in main library
    Add TwoFactorClient export to better_auth_client.dart so users can access the 2FA functionality.

--- lib/better_auth_client.dart
+++ lib/better_auth_client.dart
@@ -5,3 +5,4 @@ export 'src/auth_client.dart';
 export 'src/models/response/user.dart';
 export 'src/core/configs/enums/providers.dart';
 export './src/service/local-storage-service/better_auth_local_storage_service.dart';
+export './src/plugins/two-factor/two_factor_client.dart';
